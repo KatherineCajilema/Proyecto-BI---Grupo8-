2d3a6674b694d5da1be503f044a8bf510a808c60
KatherineCajilema <kate95kc@hotmail.com>
2018-08-07 10:23:19 -0500

    partido

---
+++ Proyecto/Analisis/partidos 28-Junio/Japon-Polonia/JaponCoordenadas.py
@@ -0,0 +1,49 @@
+__author__ = 'diegoguaman'
+
+'''
+
+
+ QUITO
+==============
+'''
+import couchdb
+import sys
+import urllib2
+import re
+import json
+
+regex = re.compile(r'[A-z a-z 0-9]+')
+
+from couchdb import view
+
+URL = 'localhost'
+db_name = 'japon'
+
+
+'''========couchdb'=========='''
+server = couchdb.Server('http://localhost:5984/')  #('http://245.106.43.184:5984/') poner la url de su base de datos
+try:
+    print (db_name)
+    db = server[db_name]
+    print ('success')
+
+except:
+    sys.stderr.write("Error: DB not found. Closing...\n")
+    sys.exit()
+
+
+
+url = 'http://127.0.0.1:5984/japon/_design/japon28JuneCoordenadasOp/_view/japon28JuneCoordenadasOp'
+req = urllib2.Request(url)
+f = urllib2.urlopen(req)
+d = json.loads(f.read())
+data = {}
+data ['people'] = []
+for x in d['rows']:
+	a = x ['value']
+
+	data ['people'].append({'text': x })
+	with open ('CoordenadasJapon.txt' , 'w') as outfile:
+		json.dump(data,outfile)
+		print (data)
+f.close()
